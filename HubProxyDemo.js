dc058afd2f9f35c662e44e27acc7f5d1e2d61134
Justin Pradier <chromecide@chromecide.com>
2015-06-09 08:16:59 +1000

    committing folder cleanup

--- HubProxyDemo.js
+++
@@ -1,7 +0,0 @@
-var ThingHttpProxy = require(__dirname+"/lib/proxyHttp.js");
-
-var hub = new ThingHttpProxy("http://locahost:3000");
-
-new ThingHttpProxy("http://localhost:3000", function(err, hub){
-    console.log(hub);
-});
